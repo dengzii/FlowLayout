6cb24b19f95b5fc573f71bb222a35fdb5532f1fe
dengzii <dengzii@foxmail.com>
2020-09-22 22:01:45 +0800

    init

---
+++ app/src/main/java/com/dengzii/flowlayout/app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dengzii.flowlayout.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dengzii.flowlayout.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ flowlayout/src/main/java/com/dengzii/flowlayout/FlowLayout.kt
@@ -0,0 +1,24 @@
+package com.dengzii.flowlayout
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
+
+class FlowLayout : ViewGroup {
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+
+    override fun onLayout(p0: Boolean, p1: Int, p2: Int, p3: Int, p4: Int) {
+
+    }
+}
